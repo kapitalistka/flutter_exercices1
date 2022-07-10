dc19d37f9f720d38ad5f787b05bc0306323c27de
Irina Masloed <irina.masloed@ooma.com>
2022-07-10 23:18:50 +0300

    Homework 2

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,51 @@
-import 'ex1.dart';
+// Flutter imports:
+import 'package:flutter/material.dart';
+
+// Package imports:
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 void main() {
+  runApp(const MyApp());
+}
 
-  //1.
-  /*print(gcd(0, 20));
-  print(lcm(12, 18));
-  print(factorize(30));*/
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  static const String assetName = 'assets/images/hat.svg';
 
+
+  @override
+  Widget build(BuildContext context) {
+
+    return MaterialApp(
+      title: 'Welcome to Flutter',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Welcome to Flutter'),
+        ),
+        body:  Center(
+          child: CarouselSlider(
+            options: CarouselOptions(height: 400.0),
+            items: [1,2,3,4,5].map((i) {
+              return Builder(
+                builder: (BuildContext context) {
+                  return Container(
+                      width: MediaQuery.of(context).size.width,
+                      margin: EdgeInsets.symmetric(horizontal: 5.0),
+                      decoration: BoxDecoration(
+                          color: Colors.deepPurple
+                      ),
+                      child: SvgPicture.asset(
+                          assetName,
+                          semanticsLabel: 'Logo'
+                      )
+                  );
+                },
+              );
+            }).toList(),
+          ),
+        ),
+      ),
+    );
+  }
 }
